cee696e857d0652c345e4ff7e5351d35c076a74f
aaronsleepy <aaron@kmong.com>
2022-11-20 20:28:25 +0900

    refactoring: DiffableDataSource, Snapshot, CompositionalLayout 사용하도록 변경

--- GridViewDemo/GridViewDemoViewController.swift
+++ GridViewDemo/GridViewDemoViewController.swift
@@ -10,12 +10,54 @@ import UIKit
 class GridViewDemoViewController: UIViewController {
     let frameworks: [AppleFrameworkModel] = AppleFrameworkModel.list
     
+    // Datasource
+    var dataSource: UICollectionViewDiffableDataSource<Section, Item>!
+    typealias Item = AppleFrameworkModel
+    enum Section {
+        case main
+    }
+    
+    
+
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
         collectionView.delegate = self
+        
+        dataSource = UICollectionViewDiffableDataSource<Section, Item>(collectionView: collectionView, cellProvider: { collectionView, indexPath, item in
+            guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridViewCollectionViewCell", for: indexPath) as? GridViewCollectionViewCell else {
+                return nil
+            }
+            cell.configigure(item)
+            return cell
+        })
+        
+        var snapshot = NSDiffableDataSourceSnapshot<Section, Item>()
+        snapshot.appendSections([.main])
+        snapshot.appendItems(frameworks, toSection: .main)
+        
+        dataSource.apply(snapshot)
+        
+        collectionView.collectionViewLayout = layout()
      }
+    
+    private func layout() -> UICollectionViewCompositionalLayout {
+        // item
+        let itemSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(0.33), heightDimension: .fractionalHeight(1))
+        let item = NSCollectionLayoutItem(layoutSize: itemSize)
+        
+        // group
+        let groupSize = NSCollectionLayoutSize(widthDimension: .fractionalWidth(1), heightDimension: .fractionalWidth(0.33))
+        let group = NSCollectionLayoutGroup.horizontal(layoutSize: groupSize, subitem: item, count: 3)
+        
+        // section
+        let section = NSCollectionLayoutSection(group: group)
+        
+        let layout = UICollectionViewCompositionalLayout(section: section)
+        
+        return layout
+    }
 }
 
 extension GridViewDemoViewController: UICollectionViewDelegate {
