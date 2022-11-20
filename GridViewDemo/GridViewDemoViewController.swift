3bbcdafd83c226859293a36223216fb98237364b
aaronsleepy <aaron@kmong.com>
2022-11-20 20:01:51 +0900

    refactor: dataSource, FlowLayout 라인 제거

--- GridViewDemo/GridViewDemoViewController.swift
+++ GridViewDemo/GridViewDemoViewController.swift
@@ -10,51 +10,12 @@ import UIKit
 class GridViewDemoViewController: UIViewController {
     let frameworks: [AppleFrameworkModel] = AppleFrameworkModel.list
     
-
     @IBOutlet weak var collectionView: UICollectionView!
     override func viewDidLoad() {
         super.viewDidLoad()
         
-        collectionView.dataSource = self
         collectionView.delegate = self
-        
-        collectionView.contentInset = UIEdgeInsets(top: 20, left: 16, bottom: 0, right: 16)
      }
-}
-
-extension GridViewDemoViewController : UICollectionViewDataSource {
-    func collectionView(_ collectionView: UICollectionView, numberOfItemsInSection section: Int) -> Int {
-        return frameworks.count
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, cellForItemAt indexPath: IndexPath) -> UICollectionViewCell {
-        guard let cell = collectionView.dequeueReusableCell(withReuseIdentifier: "GridViewCollectionViewCell", for: indexPath) as? GridViewCollectionViewCell else {
-            return UICollectionViewCell()
-        }
-        
-        cell.configigure(frameworks[indexPath.item])
-        
-        return cell
-    }
-}
-
-extension GridViewDemoViewController : UICollectionViewDelegateFlowLayout {
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, sizeForItemAt indexPath: IndexPath) -> CGSize {
-        let interItemSpacing: CGFloat = 10
-        let padding: CGFloat = 16
-        let width = (collectionView.bounds.width  - interItemSpacing * 2 - padding * 2) / 3
-        let height = width * 1.5
-        
-        return CGSize(width: width, height: height)
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumInteritemSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
-    
-    func collectionView(_ collectionView: UICollectionView, layout collectionViewLayout: UICollectionViewLayout, minimumLineSpacingForSectionAt section: Int) -> CGFloat {
-        return 10
-    }
 }
 
 extension GridViewDemoViewController: UICollectionViewDelegate {
