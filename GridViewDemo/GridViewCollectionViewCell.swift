1a4cfcc26186e4e426d8ca941c8a74190b3cfa74
aaronsleepy <aaron@kmong.com>
2022-11-20 10:33:30 +0900

    assets: 데모 Apple Framework assets 추가

---
+++ GridViewDemo/GridViewCollectionViewCell.swift
@@ -0,0 +1,16 @@
+//
+//  GridViewCollectionViewCell.swift
+//  GridViewDemo
+//
+//  Created by Aaron on 2022/11/20.
+//
+
+import UIKit
+
+class GridViewCollectionViewCell: UICollectionViewCell {
+    
+    @IBOutlet weak var thumbnailImageView: UIImageView!
+    @IBOutlet weak var nameLabel: UILabel!
+    
+    
+}
