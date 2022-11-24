69898de9e89d0fe6f63c90be716b74a57edc8029
aaronsleepy <aaron@kmong.com>
2022-11-24 19:00:23 +0900

    ui: Detail View 디자인

---
+++ GridViewDemo/FrameworkDetailViewController.swift
@@ -0,0 +1,29 @@
+//
+//  FrameworkDetailViewController.swift
+//  GridViewDemo
+//
+//  Created by Aaron on 2022/11/24.
+//
+
+import UIKit
+
+class FrameworkDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
